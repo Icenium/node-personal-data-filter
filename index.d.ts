eb1ecbd542b71b57105bd5206b86f46a84c1b3a9
Martin Bektchiev <martin.bektchiev@progress.com>
2020-08-21 10:06:03 +0300

    chore: Convert tabs to 4 spaces

--- index.d.ts
+++ index.d.ts
@@ -1,54 +1,54 @@
 declare module PersonalDataFilter {
-	interface IPersonalDataFilter {
-		filter(data: any): any;
-	}
+    interface IPersonalDataFilter {
+        filter(data: any): any;
+    }
 
 
-	interface IPersonalDataFilterConfig {
-		/**
-		 * Regular expressions which will be added to the default ones.
-		 */
-		additionalRegularExpressions?: string[];
+    interface IPersonalDataFilterConfig {
+        /**
+         * Regular expressions which will be added to the default ones.
+         */
+        additionalRegularExpressions?: string[];
 
-		/**
-		 * Regular expression which will be used instead of the default one.
-		 */
-		regularExpression?: RegExp;
+        /**
+         * Regular expression which will be used instead of the default one.
+         */
+        regularExpression?: RegExp;
 
-		/**
-		 * String which will be used to replace the personal data.
-		 */
-		personalDataMask?: string;
+        /**
+         * String which will be used to replace the personal data.
+         */
+        personalDataMask?: string;
 
-		/**
-		 * Personal data properties which will be added to the default ones.
-		 */
-		additionalPersonalDataProperties?: string[];
+        /**
+         * Personal data properties which will be added to the default ones.
+         */
+        additionalPersonalDataProperties?: string[];
 
-		/**
-		 * Personal data properties which will be used instead of the default ones.
-		 */
-		personalDataProperties?: string[];
+        /**
+         * Personal data properties which will be used instead of the default ones.
+         */
+        personalDataProperties?: string[];
 
-		/**
-		 * Defines if the filter should use the default match replacer to replace each regular
-		 * expression match.
-		 */
-		useDefaultMatchReplacer?: boolean;
+        /**
+         * Defines if the filter should use the default match replacer to replace each regular
+         * expression match.
+         */
+        useDefaultMatchReplacer?: boolean;
 
-		/**
-		 * Sets the match replacer which will be used to replace each regular
-		 * expression match.
-		 */
-		matchReplacer: MatchReplacer;
-	}
+        /**
+         * Sets the match replacer which will be used to replace each regular
+         * expression match.
+         */
+        matchReplacer: MatchReplacer;
+    }
 
-	/**
-	 * Function which will be used to replace each regular expression match.
-	 */
-	type MatchReplacer = (match: string) => string
+    /**
+     * Function which will be used to replace each regular expression match.
+     */
+    type MatchReplacer = (match: string) => string
 }
 
 declare module "node-personal-data-filter" {
-	export const newFilter: (cfg?: PersonalDataFilter.IPersonalDataFilterConfig) => PersonalDataFilter.IPersonalDataFilter;
+    export const newFilter: (cfg?: PersonalDataFilter.IPersonalDataFilterConfig) => PersonalDataFilter.IPersonalDataFilter;
 }
--- test/personal-data-filter.js
+++ test/personal-data-filter.js
@@ -8,182 +8,182 @@ const expectedMaskedOutput = "*****";
 const personalDataFilter = pdf.newFilter({ personalDataMask: expectedMaskedOutput });
 
 describe("PersonalDataFilter", () => {
-	describe("filter", () => {
-		const email = "qweasd12_=+3!#$%^&@ice.cold";
-		const guid = "1fec999a-7e81-4bce-8b32-1b6ddd144f1b";
-		const ipV4 = "169.254.139.119";
-		const ipV6 = "fe80::f991:38d8:27e6:8b77";
-		const notPersonalData = "not-personal-data";
-		const secondPropertyName = "second";
-		const emailMixedCasePropertyName = "eMaIl";
+    describe("filter", () => {
+        const email = "qweasd12_=+3!#$%^&@ice.cold";
+        const guid = "1fec999a-7e81-4bce-8b32-1b6ddd144f1b";
+        const ipV4 = "169.254.139.119";
+        const ipV6 = "fe80::f991:38d8:27e6:8b77";
+        const notPersonalData = "not-personal-data";
+        const secondPropertyName = "second";
+        const emailMixedCasePropertyName = "eMaIl";
 
-		describe("properties", () => {
-			const personalDataProperties = ["Authorization", "email", "useremail", "user", "username", "userid", "accountid", "account", "password", "pass", "pwd", "ip", "ipaddress"];
+        describe("properties", () => {
+            const personalDataProperties = ["Authorization", "email", "useremail", "user", "username", "userid", "accountid", "account", "password", "pass", "pwd", "ip", "ipaddress"];
 
-			personalDataProperties.forEach(p => {
-				it(`should filter ${p} on first level`, () => {
-					const data = {};
-					data[p] = "personal-data";
+            personalDataProperties.forEach(p => {
+                it(`should filter ${p} on first level`, () => {
+                    const data = {};
+                    data[p] = "personal-data";
 
-					const result = personalDataFilter.filter(data);
+                    const result = personalDataFilter.filter(data);
 
-					const expected = {};
-					expected[p] = expectedMaskedOutput;
-					assert.deepEqual(result, expected);
-				});
+                    const expected = {};
+                    expected[p] = expectedMaskedOutput;
+                    assert.deepEqual(result, expected);
+                });
 
-				it(`should filter ${p} on second level`, () => {
-					const data = {};
-					data[secondPropertyName] = {};
-					data[secondPropertyName][p] = "personal-data";
+                it(`should filter ${p} on second level`, () => {
+                    const data = {};
+                    data[secondPropertyName] = {};
+                    data[secondPropertyName][p] = "personal-data";
 
-					const result = personalDataFilter.filter(data);
+                    const result = personalDataFilter.filter(data);
 
-					const expected = {};
-					expected[secondPropertyName] = {};
-					expected[secondPropertyName][p] = expectedMaskedOutput;
-					assert.deepEqual(result, expected);
-				});
-			});
+                    const expected = {};
+                    expected[secondPropertyName] = {};
+                    expected[secondPropertyName][p] = expectedMaskedOutput;
+                    assert.deepEqual(result, expected);
+                });
+            });
 
-			it("should search for properties case insensitive", () => {
-				const data = {};
-				data[emailMixedCasePropertyName] = "personal-data";
+            it("should search for properties case insensitive", () => {
+                const data = {};
+                data[emailMixedCasePropertyName] = "personal-data";
 
-				const result = personalDataFilter.filter(data);
+                const result = personalDataFilter.filter(data);
 
-				const expected = {};
-				expected[emailMixedCasePropertyName] = expectedMaskedOutput;
+                const expected = {};
+                expected[emailMixedCasePropertyName] = expectedMaskedOutput;
 
-				assert.deepEqual(result, expected);
-			});
+                assert.deepEqual(result, expected);
+            });
 
-			it("should check the values of all properties, not only the ones in the list.", () => {
-				const data = {
-					filterMe: email,
-					dontFilterMe: notPersonalData,
-					nextLevel: {
-						filterMe: guid,
-						dontFilterMe: notPersonalData,
-						email
-					},
-					email
-				};
+            it("should check the values of all properties, not only the ones in the list.", () => {
+                const data = {
+                    filterMe: email,
+                    dontFilterMe: notPersonalData,
+                    nextLevel: {
+                        filterMe: guid,
+                        dontFilterMe: notPersonalData,
+                        email
+                    },
+                    email
+                };
 
-				const result = personalDataFilter.filter(data);
+                const result = personalDataFilter.filter(data);
 
-				const expected = {
-					filterMe: expectedMaskedOutput,
-					dontFilterMe: notPersonalData,
-					nextLevel: {
-						filterMe: expectedMaskedOutput,
-						dontFilterMe: notPersonalData,
-						email: expectedMaskedOutput
-					},
-					email: expectedMaskedOutput
-				};
+                const expected = {
+                    filterMe: expectedMaskedOutput,
+                    dontFilterMe: notPersonalData,
+                    nextLevel: {
+                        filterMe: expectedMaskedOutput,
+                        dontFilterMe: notPersonalData,
+                        email: expectedMaskedOutput
+                    },
+                    email: expectedMaskedOutput
+                };
 
-				assert.deepEqual(result, expected);
-			});
-		});
+                assert.deepEqual(result, expected);
+            });
+        });
 
-		describe("strings", () => {
-			const hashMatchReplacer = (match) => crypto.createHash("sha256").update(match).digest("hex");
-			const tests = [{
-				title: "filter with default match replacer",
-				expectedTextTransformer: hashMatchReplacer,
-				filter: pdf.newFilter({ useDefaultMatchReplacer: true })
-			}, {
-				title: "filter with mask",
-				expectedTextTransformer: () => expectedMaskedOutput,
-				filter: personalDataFilter
-			}];
+        describe("strings", () => {
+            const hashMatchReplacer = (match) => crypto.createHash("sha256").update(match).digest("hex");
+            const tests = [{
+                title: "filter with default match replacer",
+                expectedTextTransformer: hashMatchReplacer,
+                filter: pdf.newFilter({ useDefaultMatchReplacer: true })
+            }, {
+                title: "filter with mask",
+                expectedTextTransformer: () => expectedMaskedOutput,
+                filter: personalDataFilter
+            }];
 
-			tests.forEach(test => {
-				describe(test.title, () => {
-					const testCaces = [
-						{ msg: email, expected: test.expectedTextTransformer(email) },
-						{ msg: `text ${email} text`, expected: `text ${test.expectedTextTransformer(email)} text` },
-						{ msg: `text ${email} ${email} text`, expected: `text ${test.expectedTextTransformer(email)} ${test.expectedTextTransformer(email)} text` },
-						{ msg: guid, expected: test.expectedTextTransformer(guid) },
-						{ msg: "487818704899480c907e2c0549664116", expected: "487818704899480c907e2c0549664116" },
-						{ msg: `text ${guid} text`, expected: `text ${test.expectedTextTransformer(guid)} text` },
-						{ msg: `${guid}${guid}`, expected: `${test.expectedTextTransformer(guid)}${test.expectedTextTransformer(guid)}` },
-						{ msg: `text${guid}text${guid}text`, expected: `text${test.expectedTextTransformer(guid)}text${test.expectedTextTransformer(guid)}text` },
-						{ msg: `text${guid}${guid}text`, expected: `text${test.expectedTextTransformer(guid)}${test.expectedTextTransformer(guid)}text` },
-						{ msg: `${email}${guid}`, expected: `${test.expectedTextTransformer(email)}${test.expectedTextTransformer(guid)}` },
-						{ msg: `${email} ${guid} ${email}`, expected: `${test.expectedTextTransformer(email)} ${test.expectedTextTransformer(guid)} ${test.expectedTextTransformer(email)}` },
-						{ msg: ipV4, expected: test.expectedTextTransformer(ipV4) },
-						{ msg: `text${ipV4}text`, expected: `text${test.expectedTextTransformer(ipV4)}text` },
-						{ msg: ipV6, expected: test.expectedTextTransformer(ipV6) },
-						{ msg: `text${ipV6}text`, expected: `text${test.expectedTextTransformer(ipV6)}text` }
-					];
+            tests.forEach(test => {
+                describe(test.title, () => {
+                    const testCaces = [
+                        { msg: email, expected: test.expectedTextTransformer(email) },
+                        { msg: `text ${email} text`, expected: `text ${test.expectedTextTransformer(email)} text` },
+                        { msg: `text ${email} ${email} text`, expected: `text ${test.expectedTextTransformer(email)} ${test.expectedTextTransformer(email)} text` },
+                        { msg: guid, expected: test.expectedTextTransformer(guid) },
+                        { msg: "487818704899480c907e2c0549664116", expected: "487818704899480c907e2c0549664116" },
+                        { msg: `text ${guid} text`, expected: `text ${test.expectedTextTransformer(guid)} text` },
+                        { msg: `${guid}${guid}`, expected: `${test.expectedTextTransformer(guid)}${test.expectedTextTransformer(guid)}` },
+                        { msg: `text${guid}text${guid}text`, expected: `text${test.expectedTextTransformer(guid)}text${test.expectedTextTransformer(guid)}text` },
+                        { msg: `text${guid}${guid}text`, expected: `text${test.expectedTextTransformer(guid)}${test.expectedTextTransformer(guid)}text` },
+                        { msg: `${email}${guid}`, expected: `${test.expectedTextTransformer(email)}${test.expectedTextTransformer(guid)}` },
+                        { msg: `${email} ${guid} ${email}`, expected: `${test.expectedTextTransformer(email)} ${test.expectedTextTransformer(guid)} ${test.expectedTextTransformer(email)}` },
+                        { msg: ipV4, expected: test.expectedTextTransformer(ipV4) },
+                        { msg: `text${ipV4}text`, expected: `text${test.expectedTextTransformer(ipV4)}text` },
+                        { msg: ipV6, expected: test.expectedTextTransformer(ipV6) },
+                        { msg: `text${ipV6}text`, expected: `text${test.expectedTextTransformer(ipV6)}text` }
+                    ];
 
-					testCaces.forEach(t => {
-						it(`should transform "${t.msg}" to "${t.expected}"`, () => {
-							const result = test.filter.filter(t.msg);
-							assert.deepEqual(result, t.expected);
-						});
-					});
-				});
-			});
-		});
+                    testCaces.forEach(t => {
+                        it(`should transform "${t.msg}" to "${t.expected}"`, () => {
+                            const result = test.filter.filter(t.msg);
+                            assert.deepEqual(result, t.expected);
+                        });
+                    });
+                });
+            });
+        });
 
-		describe("arrays", () => {
-			it("should filter all array objects.", () => {
-				const data = [email, guid, notPersonalData, { userEmail: email, filterMe: email, dontFilterMe: notPersonalData, filterMeToo: guid }];
+        describe("arrays", () => {
+            it("should filter all array objects.", () => {
+                const data = [email, guid, notPersonalData, { userEmail: email, filterMe: email, dontFilterMe: notPersonalData, filterMeToo: guid }];
 
-				const result = personalDataFilter.filter(data);
+                const result = personalDataFilter.filter(data);
 
-				const expected = [expectedMaskedOutput, expectedMaskedOutput, notPersonalData, {
-					userEmail: expectedMaskedOutput,
-					filterMe: expectedMaskedOutput,
-					dontFilterMe: notPersonalData,
-					filterMeToo: expectedMaskedOutput
-				}];
+                const expected = [expectedMaskedOutput, expectedMaskedOutput, notPersonalData, {
+                    userEmail: expectedMaskedOutput,
+                    filterMe: expectedMaskedOutput,
+                    dontFilterMe: notPersonalData,
+                    filterMeToo: expectedMaskedOutput
+                }];
 
-				assert.deepEqual(result, expected);
-			});
-		});
+                assert.deepEqual(result, expected);
+            });
+        });
 
-		describe("errors", () => {
-			it("should handle errors.", () => {
-				const test = new Error('some message');
+        describe("errors", () => {
+            it("should handle errors.", () => {
+                const test = new Error('some message');
 
-				const result = personalDataFilter.filter(test);
+                const result = personalDataFilter.filter(test);
 
-				assert.deepEqual(result.message, test.message);
-				assert.deepEqual(result.stack, test.stack);
-			});
+                assert.deepEqual(result.message, test.message);
+                assert.deepEqual(result.stack, test.stack);
+            });
 
-			it("should filter errors.", () => {
-				const test = new Error(`some message with personal data ${email}`);
+            it("should filter errors.", () => {
+                const test = new Error(`some message with personal data ${email}`);
 
-				const result = personalDataFilter.filter(test);
+                const result = personalDataFilter.filter(test);
 
-				assert.deepEqual(result.message, result.message.replace(email, expectedMaskedOutput));
-				assert.deepEqual(result.stack, result.stack.replace(email, expectedMaskedOutput));
-			});
-		});
+                assert.deepEqual(result.message, result.message.replace(email, expectedMaskedOutput));
+                assert.deepEqual(result.stack, result.stack.replace(email, expectedMaskedOutput));
+            });
+        });
 
-		describe("objects", () => {
-			it("should handle circular references.", () => {
-				const nestedObject = { userEmail: email, filterMe: email, dontFilterMe: notPersonalData, filterMeToo: guid };
-				nestedObject.circular = nestedObject;
-				const data = [email, nestedObject];
+        describe("objects", () => {
+            it("should handle circular references.", () => {
+                const nestedObject = { userEmail: email, filterMe: email, dontFilterMe: notPersonalData, filterMeToo: guid };
+                nestedObject.circular = nestedObject;
+                const data = [email, nestedObject];
 
-				const result = personalDataFilter.filter(data);
+                const result = personalDataFilter.filter(data);
 
-				const expected = [expectedMaskedOutput, {
-					userEmail: expectedMaskedOutput,
-					filterMe: expectedMaskedOutput,
-					dontFilterMe: notPersonalData,
-					filterMeToo: expectedMaskedOutput,
-					circular: expectedMaskedOutput,
-				}];
+                const expected = [expectedMaskedOutput, {
+                    userEmail: expectedMaskedOutput,
+                    filterMe: expectedMaskedOutput,
+                    dontFilterMe: notPersonalData,
+                    filterMeToo: expectedMaskedOutput,
+                    circular: expectedMaskedOutput,
+                }];
 
-				assert.deepEqual(result, expected);
-			});
-		});
-	});
+                assert.deepEqual(result, expected);
+            });
+        });
+    });
 });
